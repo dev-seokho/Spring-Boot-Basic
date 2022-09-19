3764c2a54cd133eaf2deda4b3cd0c7f8984bc673
dev-seokho <loyu78@gmail.com>
2022-09-19 15:26:37 +0900

    스프링 빈 조회 동일한 타입이 둘 이상

--- src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -40,8 +40,7 @@ public class ApplicationContextBasicFindTest {
     @Test
     @DisplayName("빈 이름으로 조회X")
     void findBeanByNameX() {
-        MemberService xxxxx =ac.getBean("xxxx", MemberService.class);
         assertThrows(NoSuchBeanDefinitionException.class,
-                () -> ac.getBean("xxxx", MemberService.class));
+                () -> ac.getBean("xxxxx", MemberService.class));
     }
 }
